8ca754c04ac94b32d818a94c841a9fc251508d5d
Elliott Barringer <elliottbarringer@icloud.org>
2024-01-30 17:03:39 -0800

    display method for Rossler system

--- src/rossler/mod.rs
+++ src/rossler/mod.rs
@@ -1,4 +1,5 @@
 pub mod rossler {
+    use plotters::prelude::*;
     pub struct RosslerSystem {
         pub x: f64,
         pub y: f64,
@@ -40,12 +41,20 @@ pub mod rossler {
         }
 
         /// Returns the points of the Rossler System after the simulation is completed
-        pub fn simulate(&self) -> (f64, f64, f64) {
+        pub fn simulate(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
+            let mut x_values: Vec<f64> = Vec::with_capacity(self.steps);
+            let mut y_values: Vec<f64> = Vec::with_capacity(self.steps);
+            let mut z_values: Vec<f64> = Vec::with_capacity(self.steps);
+
             let mut x: f64 = self.x;
             let mut y: f64 = self.y;
             let mut z: f64 = self.z;
 
             for _ in 0..self.steps {
+                x_values.push(x);
+                y_values.push(y);
+                z_values.push(z);
+
                 let dx: f64 = -y - z;
                 let dy: f64 = x + self.a * y;
                 let dz: f64 = self.b + z * (x - self.c);
@@ -55,7 +64,48 @@ pub mod rossler {
                 z = z + self.dt * dz;
             }
 
-            (x, y, z)
+            (x_values, y_values, z_values)
+        }
+        
+        /// Displays the Rossler system trajectories
+        pub fn display(&self, trajectories: (Vec<f64>, Vec<f64>, Vec<f64>)) {
+            let root: DrawingArea<BitMapBackend<'_>, plotters::coord::Shift> = BitMapBackend::new("rossler_plot.png", (800, 600)).into_drawing_area();
+            root.fill(&WHITE).unwrap();
+
+            let mut chart = ChartBuilder::on(&root)
+                .caption("Lorenz System Trajectories", ("Arial", 20).into_font())
+                .margin(5)
+                .x_label_area_size(40)
+                .y_label_area_size(40)
+                .build_cartesian_2d(0.0..self.steps as f64, -20.0..20.0)
+                .unwrap();
+
+            chart
+                .configure_mesh()
+                .x_desc("Time Step")
+                .y_desc("Values")
+                .draw()
+                .unwrap();
+
+            chart
+                .draw_series(LineSeries::new(trajectories.0.iter().enumerate().map(|(i, &val)| (i as f64, val)), &RED))
+                .unwrap()
+                .label("X Trajectory")
+                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+            chart
+                .draw_series(LineSeries::new(trajectories.1.iter().enumerate().map(|(i, &val)| (i as f64, val)), &GREEN))
+                .unwrap()
+                .label("Y Trajectory")
+                .legend(|(x, y)| PathElement::new(vec![(x, y - 10), (x + 20, y - 10)], &GREEN));
+
+            chart
+                .draw_series(LineSeries::new(trajectories.2.iter().enumerate().map(|(i, &val)| (i as f64, val)), &BLUE))
+                .unwrap()
+                .label("Z Trajectory")
+                .legend(|(x, y)| PathElement::new(vec![(x, y - 20), (x + 20, y - 20)], &BLUE));
+
+            chart.configure_series_labels().background_style(&WHITE.mix(0.8)).draw().unwrap();
         }
     }
 }
